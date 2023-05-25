27f316583d9ea6c21cb0ddc29cbdcb9117572693
czappi44 <czappadam@gmail.com>
2023-05-25 10:23:36 +0000

    added plot config

---
+++ utils/plot_config.py
@@ -0,0 +1,5 @@
+plot_line_width = 5.0
+fontsize = 30
+plot_axis_line_width = 1.5
+colors = {"color_0": "#18527c", "color_1": "#ffa655", "color_2": "lightskyblue"}
+linestyles = ['solid', 'dashed', 'dotted',  (5, (10,2)), 'dashdot']
