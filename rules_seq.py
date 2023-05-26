8d27d6baaa5fbb6d711787815f8cc17591255813
czappi44 <czappadam@gmail.com>
2023-05-26 13:33:00 +0000

    added results to rules_seq, and added dosctrings

--- rules_seq.py
+++ rules_seq.py
@@ -1,23 +1,41 @@
 import numpy as np
 import pandas as pd
 import argparse
+import os.path
 
 from split.dataset_splitter import create_l1o_split, create_adjusted_time_based_split
 from split.rules import create_sequential_rule_pairs, calculate_rule_overlap
 
-def rule_overlap(full_path, train_path, test_path, session_key='SessionId', item_key='ItemId', time_key='Time'):
+def rule_overlap(full_path:str, train_path:str, test_path:str, session_key:str='SessionId', item_key:str='ItemId', time_key:str='Time') -> pd.DataFrame:
+    """Calculates the A->B rule overlaps for different techniques on the dataset.
+
+    Args:
+        full_path (str): Path to the full dataset
+        train_path (str): Path to the train dataset
+        test_path (str): Path to the test dataset
+        session_key (str, optional): Column name of the sessin ID. Defaults to 'SessionId'.
+        item_key (str, optional): Column name of the item ID. Defaults to 'ItemId'.
+        time_key (int, optional): Column name of the timestamp. Defaults to 'Time'.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the results: dataset_name, method, overlap
+    """
+    #NOTE: It is assumed, that all 3 dataset contain sessions with at least 2 events 
+    result = pd.DataFrame({"dataset_name":[], "method":[], "overlap":[]})
+    dataset_name = os.path.split(full_path)[-1].split('_')[0]
     data_full = pd.read_csv(full_path, sep='\t', dtype={session_key:str, item_key:str, time_key:int})
-    train_l1o, test_l1o = create_l1o_split(data_full)
+    
     print("Calculating overlap for 'leave-one-out' technique")
+    train_l1o, test_l1o = create_l1o_split(data_full, duplicate_penultimate_event=True)
     train_rules_l1o = create_sequential_rule_pairs(train_l1o)
     test_rules_l1o = create_sequential_rule_pairs(test_l1o)
     overlap_l1o = calculate_rule_overlap(test_rules_l1o, train_rules_l1o)
     train_events_l1o = len(train_l1o)
     test_events_l1o = len(test_l1o)-test_l1o.loc[:,session_key].nunique()
-
     print(f"\ttrain shape: {train_l1o.shape}", f"test shape: {test_l1o.shape}", f"test events: {test_events_l1o}")
     print(f"\ttrain_ratio: {train_events_l1o/len(data_full):.4f}", f"test_ratio: {test_events_l1o/len(data_full):.4f}")
-    print(f"\toverlap full l1o: {overlap_l1o:.8f}")
+    print(f"\toverlap: {overlap_l1o:.8f}")
+    result.loc[0] = [dataset_name, "Leave-one-out", overlap_l1o]
 
     print(f"Calculating overlap for 'timebased split' technique, with test size adjusted to l1o test size {test_events_l1o/len(data_full):.4f}")
     train_tb, test_tb = create_adjusted_time_based_split(data_full, test_events=test_events_l1o)
@@ -26,17 +44,35 @@ def rule_overlap(full_path, train_path, test_path, session_key='SessionId', item
     overlap_tb = calculate_rule_overlap(test_rules_tb, train_rules_tb)
     train_events_tb = len(train_tb)
     test_events_tb = len(test_tb)-test_tb.loc[:,session_key].nunique()
-
-    print(f"\ttrain shape: {train_tb.shape}", f"\ttest shape: {test_tb.shape}", f"test events: {test_events_tb}")
+    print(f"\ttrain shape: {train_tb.shape}", f"test shape: {test_tb.shape}", f"test events: {test_events_tb}")
     print(f"\ttrain_ratio: {train_events_tb/len(data_full):.4f}", f"test_ratio: {test_events_tb/len(data_full):.4f}")
-    print(f"\toverlap full l1o: {overlap_tb:.8f}")
+    print(f"\toverlap: {overlap_tb:.8f}")
+    result.loc[1] = [dataset_name, "Time based", overlap_tb]
 
-    # train_tb = pd.read_csv(train_path, sep='\t', dtype={session_key:str, item_key:str, time_key:int})
-    # test_tb = pd.read_csv(test_path, sep='\t', dtype={session_key:str, item_key:str, time_key:int})
-    # train_rules_tb = create_sequential_rule_pairs(train_tb)
-    # test_rules_tb = create_sequential_rule_pairs(test_tb)
-    # overlap_tb = calculate_overlap(test_rules_tb, train_rules_tb)
-    # print(f"overlap timebased original: {overlap_tb:.8f}")
+    print(f"Calculating overlap for 'leave-one-out with timebased split' technique")
+    train = pd.read_csv(train_path, sep='\t', dtype={session_key:str, item_key:str, time_key:int})
+    test = pd.read_csv(test_path, sep='\t', dtype={session_key:str, item_key:str, time_key:int})
+    _, test_l1o_tb = create_l1o_split(test, force_test=True, duplicate_penultimate_event=True)
+    train_rules = create_sequential_rule_pairs(train)
+    test_rules_l1o_tb = create_sequential_rule_pairs(test_l1o_tb)
+    overlap_l1o_tb = calculate_rule_overlap(test_rules_l1o_tb, train_rules)
+    test_events_l1o_tb = len(test_l1o_tb)-test_l1o_tb.loc[:,session_key].nunique()
+    print(f"\ttrain shape: {train.shape}", f"test shape: {test_l1o_tb.shape}", f"test events: {test_events_l1o_tb}")
+    print(f"\toverlap: {overlap_l1o_tb:.8f}")
+    result.loc[2] = [dataset_name, "Leave-one-out: time split", overlap_l1o_tb]
+    
+    print(f"Calculating overlap for 'leave-one-out with random split' technique")
+    n_session_sample = test_l1o_tb.loc[:, session_key].nunique()
+    train_l1o_random, test_l1o_random = create_l1o_split(data_full, force_test=True, duplicate_penultimate_event=True, n_session_sample=n_session_sample)
+    train_rules_l1o_random = create_sequential_rule_pairs(train_l1o_random)
+    test_rules_l1o_random = create_sequential_rule_pairs(test_l1o_random)
+    overlap_l1o_random = calculate_rule_overlap(test_rules_l1o_random, train_rules_l1o_random)
+    test_events_l1o_random = len(test_l1o_random) - test_l1o_random.loc[:, session_key].nunique()
+    print(f"\ttrain shape: {train_l1o_random.shape}", f"test shape: {test_l1o_random.shape}", f"test events: {test_events_l1o_random}")
+    print(f"\toverlap: {overlap_l1o_random:.8f}")
+    result.loc[3] = [dataset_name, "Leave-one-out: random", overlap_l1o_random]
+
+    return result
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -44,5 +80,5 @@ if __name__ == '__main__':
     parser.add_argument('--train_path', type=str)
     parser.add_argument('--test_path', type=str)
     args = parser.parse_args()
-
-    rule_overlap(args.full_path, args.train_path, args.test_path)
+    result = rule_overlap(args.full_path, args.train_path, args.test_path)
+    print(result)
--- split/dataset_splitter.py
+++ split/dataset_splitter.py
@@ -1,19 +1,53 @@
 import numpy as np
 import pandas as pd
+from typing import Tuple
 
-def create_l1o_split(df, session_key='SessionId', item_key='ItemId', time_key='Time'):
+def create_l1o_split(df: pd.DataFrame, force_test:bool=False, duplicate_penultimate_event:bool=False, n_session_sample:int=None, session_key:str='SessionId', item_key:str='ItemId', time_key:int='Time') -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Splits a dataframe to train and test sets using leave-one-out technique.
+
+    Args:
+        df (pd.DataFrame): The data to be split
+        force_test (bool, optional): Whether to force session with length 2 to the test set. If False, only sessions with at least 3 events will be considered when selectig the test set. Defaults to False.
+        duplicate_penultimate_event (bool, optional): If True, in each session, the penultimate event will be present in both the train and test set. Defaults to False.
+        n_session_sample (int, optional): When provided, the test set will contain n_session_sample randomly sampled sessions. Defaults to None.
+        session_key (str, optional): Column name of the sessin ID. Defaults to 'SessionId'.
+        item_key (str, optional): Column name of the item ID. Defaults to 'ItemId'.
+        time_key (int, optional): Column name of the timestamp. Defaults to 'Time'.
+
+    Returns:
+        Tuple[pd.DataFrame, pd.DataFrame]: The train and test data
+    """
     test_mask = np.concatenate((df.loc[:, session_key].values[:-1] != df.loc[:, session_key].values[1:], [True]))
     session_lengths = df.groupby([session_key]).agg(sl=(item_key,'count'))
-    true_test_sessions = session_lengths[session_lengths.sl>2].index.values
+    if not force_test:
+        true_test_sessions = session_lengths[session_lengths.sl>2].index.values
+    else:
+        true_test_sessions = session_lengths.index.values
+    if n_session_sample is not None:
+        np.random.seed(42)
+        true_test_sessions = np.random.choice(true_test_sessions, size=n_session_sample, replace=False)
     true_test_sessions_mask  = df.loc[:,session_key].isin(true_test_sessions).values
     test_mask = test_mask & true_test_sessions_mask
-    train = df[~test_mask].copy()
+    train = df[~test_mask]
     session_end_idx = np.nonzero(test_mask)[0]
-    test_mask[session_end_idx-1] = True
-    test = df[test_mask].copy()
+    if duplicate_penultimate_event:
+        test_mask[session_end_idx-1] = True
+    test = df[test_mask]
     return train, test
 
-def create_adjusted_time_based_split(df, test_events, session_key='SessionId', item_key='ItemId', time_key='Time'):
+def create_adjusted_time_based_split(df:pd.DataFrame, test_events:int, session_key:str='SessionId', item_key:str='ItemId', time_key:str='Time') -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Splits a dataframe to train and test sets, with test events matching the test_events parameter, using timebased split technique.
+
+    Args:
+        df (pd.DataFrame): The data to be split
+        test_events (int): The number of events to be scored (:discounting the starting event of each session) in the test set   
+        session_key (str, optional): Column name of the sessin ID. Defaults to 'SessionId'.
+        item_key (str, optional): Column name of the item ID. Defaults to 'ItemId'.
+        time_key (int, optional): Column name of the timestamp. Defaults to 'Time'.
+
+    Returns:
+        Tuple[pd.DataFrame, pd.DataFrame]: The train and test data
+    """
     sbeg_slen = df.groupby([session_key], as_index=False).agg(sbeg=(time_key,"min"), slen=(session_key, 'count'))
     sbeg_slen = sbeg_slen.sort_values(by=["sbeg"], ascending=False)
     sbeg_slen["total_events"] = np.cumsum(sbeg_slen.slen.values - 1) # -1 for start item
