116efe2026fbef807d37101f98399ead94f109ef
czappi44 <czappadam@gmail.com>
2023-06-06 16:19:29 +0000

    modified inference sampling notebooks

--- sampling/inference_sampling.py
+++ sampling/inference_sampling.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("./GRU4Rec")
 import numpy as np, pandas as pd
 import gru4rec
-from sampling import evaluation_utils, test_item_file_builder
+import evaluation_utils, test_item_file_builder
 import os
 import joblib
 import argparse
@@ -25,16 +25,20 @@ def recall_mrr_from_ranks(ranks, nsample):
 		print(f"\tMRR@{i+1}\t\t", f"{mrr[i]:.8f}")
 	return recall, mrr
 
-def inference_with_sampling(args):
-	test_file_path = args.test_path
-	model_name = os.path.split(args.model_path)[-1][:-7]
+def inference_with_sampling(test_file_path, train_path, model_path, methods):
+	model_name = os.path.split(model_path)[-1][:-7]
 
 	test = pd.read_csv(test_file_path, sep='\t', dtype={'ItemId':'str'})
 	test_path, test_file = os.path.split(test_file_path)
-	gru = gru4rec.GRU4Rec.loadmodel(args.model_path)
+	gru = gru4rec.GRU4Rec.loadmodel(model_path)
 	s_random_names = ['full', '0.1', '0.01', '0.001', '100']
 	s_sim_names = ["closest", "farthest", "similar", "dissimilar", "uniform", "popular", "invpopular", "popstatic"]
-	for sname in  s_random_names + s_sim_names:
+	for sname in  methods: #s_random_names + s_sim_names:
+		recall_result_file = os.path.join('data', 'results', 'sampling_results', model_name + '_' + sname + '_recall.tsv')
+		mrr_result_file = os.path.join('data', 'results', 'sampling_results', model_name + '_' + sname + '_mrr.tsv')
+		if os.path.isfile(recall_result_file) and os.path.isfile(mrr_result_file):
+			print(f"Files already exist, SKIPPING:\n{recall_result_file}\n{mrr_result_file}")
+			continue
 		if not os.path.isfile(os.path.join('data', 'results', 'sampling_results', model_name + '_' + sname + '_rank.pickle')):
 			print('Building rank file for {} for {} samples'.format(model_name, sname))
 			if sname == 'full':
@@ -46,13 +50,15 @@ def inference_with_sampling(args):
 					nsample = int(float(sname) * len(gru.itemidmap))
 				np.random.seed(42)
 				ranks = evaluation_utils.get_rank_uniform(gru, test, nsample, mode='conservative')
-			else:
+			elif sname in s_sim_names:
 				nsample = 100
 				negative_items_file = model_name + '_' + sname
 				if not os.path.isfile(os.path.join(test_path, negative_items_file + ".tsv")):
 					print(f"Building negative item files for: {s_sim_names}")
-					test_item_file_builder.create_test_items(gru, n=nsample, train_path=args.train_path, test_path=test_file_path, out_path_prefix=os.path.join(test_path, model_name))
+					test_item_file_builder.create_test_items(gru, n=nsample, train_path=train_path, test_path=test_file_path, out_path_prefix=os.path.join(test_path, model_name))
 				ranks = evaluation_utils.get_rank_sampling(gru, test, mode='conservative', negative_items_file=os.path.join(test_path, negative_items_file + ".tsv"))
+			else:
+				raise ValueError(f"Invalid method name must be chosen from: {s_random_names+s_sim_names} but got {sname}")
 			joblib.dump(ranks, os.path.join('data', 'results', 'sampling_results', model_name + '_' + sname + '_rank.pickle'))
 		else:
 			ranks = joblib.load(os.path.join('data', 'results', 'sampling_results', model_name + '_' + sname + '_rank.pickle'))
@@ -64,14 +70,15 @@ def inference_with_sampling(args):
 		else:
 			nsample = int(float(sname) * len(gru.itemidmap))
 		recall, mrr = recall_mrr_from_ranks(ranks=ranks, nsample=nsample)
-		pd.DataFrame(data=recall).to_csv(os.path.join('data', 'results', 'sampling_results', model_name + '_' + sname + '_recall.tsv'), index=False, header=None)
-		pd.DataFrame(data=mrr).to_csv(os.path.join('data', 'results', 'sampling_results', model_name + '_' + sname + '_mrr.tsv'), index=False, header=None)
+		pd.DataFrame(data=recall).to_csv(recall_result_file, index=False, header=None)
+		pd.DataFrame(data=mrr).to_csv(mrr_result_file, index=False, header=None)
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--test_path', type=str)
 	parser.add_argument('--train_path', type=str)
 	parser.add_argument('--model_path', type=str)
+	parser.add_argument('--methods', type=str, nargs='+', default=['full', '0.1', '0.01', '0.001', '100', "closest", "farthest", "similar", "dissimilar", "uniform", "popular", "invpopular", "popstatic"])
 	args = parser.parse_args()
 
-	inference_with_sampling(args)
+	inference_with_sampling(test_file_path=args.test_path, train_path=args.train_path, model_path=args.model_path, methods=args.methods)
